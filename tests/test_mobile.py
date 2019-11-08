048f630c1cc08f1468dc33b52cca35f064564210
haslem <haslem11@gmail.com>
2019-11-08 15:45:53 +0100

    add_logInOut_test

--- tests/test_mobile.py
+++ tests/test_mobile.py
@@ -2,53 +2,69 @@ import json
 import pytest
 from appium import webdriver
 
-import locators_mobile
-
-from appium.webdriver.common.touch_action import TouchAction
-
 from screens.main_screen import MainScreen
 from screens.menu_screen import MenuScreen
+from screens.login_screen import LogInScreen
 
 
 @pytest.fixture(scope='session')
 def desired_cap():
-	with open('desired_cap.json') as config_file:
-		data = json.load(config_file)
-	return data	
-
+    with open('desired_cap.json') as config_file:
+        data = json.load(config_file)
+    return data
 
 
 @pytest.fixture
 def mobile(desired_cap):
-	desired_cap = desired_cap
+    desired_cap = desired_cap
 
-	driver = webdriver.Remote("http://localhost:4723/wd/hub", desired_cap)
-	driver.implicitly_wait(10)
-	yield driver
-	driver.quit()
-
+    driver = webdriver.Remote("http://localhost:4723/wd/hub", desired_cap)
+    driver.implicitly_wait(10)
+    yield driver
+    driver.quit()
 
 
 def test_menu_button(mobile):
-	# main_screen = mobile
-
-	# elem = mobile.find_element_by_id(locators_mobile.map_screen['menu']).click()
-
+    main_screen = MainScreen(mobile)
+    main_screen.menu_click()
 
 
-	# elem = mobile.find_element_by_id('cz.seznam.mapy:id/content')
-	# TouchAction(mobile).long_press(elem).move_to(x=100, y=100).release().perform()
-	main_screen = MainScreen(mobile)
-	main_screen.menu_click()
+def test_offlinemaps_button(mobile):
+    main_screen = MainScreen(mobile)
+    main_screen.menu_click()
+
+    menu_screen = MenuScreen(mobile)
+    menu_screen.offline_maps()
 
 
+def test_login(mobile):
+    USER_NAME = 'mapytesting2'
+    PASSWORD = 'testingmapy'
+
+    main_screen = MainScreen(mobile)
+    main_screen.menu_click()
+
+    menu_screen = MenuScreen(mobile)
+    menu_screen.log_in()
+
+    login_screen = LogInScreen(mobile)
+    login_screen.user_name(USER_NAME)
+    login_screen.password(PASSWORD)
+    login_screen.sign_in_button()
+
+    user_name = mobile.find_element_by_id('cz.seznam.mapy:id/userName').get_attribute('text')
+    password = mobile.find_element_by_id('cz.seznam.mapy:id/accountName').get_attribute('text')
+
+    assert user_name == 'mapytesting2'
+    assert password == 'mapytesting2@seznam.cz'
 
 
-	menu_screen = MenuScreen(mobile)
-	menu_screen.offline_maps()
+def test_logout(mobile):
+    main_screen = MainScreen(mobile)
+    main_screen.menu_click()
 
-	# elements = mobile.find_elements_by_class_name('android.widget.Button')
-	# for i in elements:
-	# 	if i.get_attribute('text') == 'Offline maps':
-	# 		i.click()
-	# 		break
+    menu_screen = MenuScreen(mobile)
+    menu_screen.log_out()
+
+    user_name = mobile.find_element_by_id('cz.seznam.mapy:id/userName').get_attribute('text')
+    assert user_name == 'Log in'
